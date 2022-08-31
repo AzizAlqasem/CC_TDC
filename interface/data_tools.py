8c42b409e199e4fc460450439a97ee95cae9785e
Alqasem <alqasem.2@asc.ohio-state.edu>
2022-08-31 14:02:03 -0400

    Numba to read data

--- interface/data_tools.py
+++ interface/data_tools.py
@@ -1,5 +1,6 @@
 from settings.settings import settings
 import numpy as np
+import numba
 
 def raw_data_to_channel_arr(
         data:list, channel_size=settings.get_setting("tot_nodch"),
@@ -15,7 +16,8 @@ def raw_data_to_channel_arr(
     return data_ar[:,1:-1]
 
 
-def channel_arr_to_count_ar(channel_arr, min_count_value=1, max_count_value=2048): #* max_count is variable => should be in the settings
+@numba.jit(nopython=True)
+def channel_arr_to_count_ar(channel_arr, min_count_value=1, max_count_value=2048):
     # the channel_arr is a 2D array with shape (number_of_data_chunck, channel_size)
     # it contains the index numbers that correspond to the hit times
     # Example: for 1 ns resoultion TDC, index at 10 ~= 10 ns
@@ -25,9 +27,10 @@ def channel_arr_to_count_ar(channel_arr, min_count_value=1, max_count_value=2048
     return _count_to_bins(data_ar, size=max_count_value - min_count_value), data_ar.size
 
 
+@numba.jit(nopython=True)
 def _count_to_bins(data_ar, size):
     count_ar = np.zeros(size, dtype=np.int32)#np.zeros(size, dtype=np.int64)
-    for i in range(len(data_ar)):
-        bin = data_ar[i]#data_ar[i]
+    for bin in data_ar:  #i in range(len(data_ar)):
+        # bin = data_ar[i]#data_ar[i]
         count_ar[bin-1] += 1
     return count_ar
